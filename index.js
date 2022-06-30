378c70c5811480fa2c64098d84f08e95b50dabc7
AlexGMarinho <alexg.marinhoooo>
2022-06-30 15:02:46 -0300

    index.js

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ let numero_da_tabuada = readlineSync.question(`Digite o valor da tabuada: `);
 
 for (let contador = 1; contador <= 10; contador++) {
     let valor_da_tabuada = contador * numero_da_tabuada;
+
     console.log(`${numero_da_tabuada} X ${contador} = ${valor_da_tabuada.toFixed(2)}`);
 }
