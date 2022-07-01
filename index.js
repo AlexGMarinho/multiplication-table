f79f50de82fa3cf54ad90a38376a11437026dc43
AlexGMarinho <alexg.marinhoooo>
2022-07-01 00:57:40 -0300

    index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-let numero_da_tabuada = readlineSync.question(`Digite o valor da tabuada: `);
+let numero_da_tabuada = readlineSync.questionFloat(`Digite o valor da tabuada: `);
 
 for (let contador = 1; contador <= 10; contador++) {
     let valor_da_tabuada = contador * numero_da_tabuada;
