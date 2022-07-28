da1a35c4a9a01dd5d763ca0dc764611ae0e194f4
AlexGMarinho <alexg.marinhoooo>
2022-07-28 19:54:19 -0300

    index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-import readlineSync from 'readline-sync';
-
-let numero_da_tabuada = readlineSync.questionFloat(`Digite o valor da tabuada: `);
-
-for (let contador = 1; contador <= 10; contador++) {
-    let valor_da_tabuada = contador * numero_da_tabuada;
-
-    console.log(`${numero_da_tabuada} X ${contador} = ${valor_da_tabuada}`);
-}
